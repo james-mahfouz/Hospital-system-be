40a11afc2d81a22a6e19fd8654bb2961025d812c
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-11 19:32:08 +0200

    added the assign_patient.php api

--- assign_patient.php
+++ assign_patient.php
@@ -5,10 +5,17 @@ require_once('vendor/autoload.php');
 use \Firebase\JWT\JWT;
 
 if (isset($_POST['jwt'], $_POST['patient_id'], $_POST['hospital_id'])) {
+    $jwt = $_POST['jwt'];
+    $patient_id = $_POST['patient_id'];
+    $hospital_id = $_POST['hospital_id'];
     
+    if ($success) {
+        $response = array('success' => true, 'message' => 'Patient assigned to hospital successfully');
+    } else {
+        $response = array('success' => false, 'message' => 'Failed to assign patient to hospital');
+    }
     
-    assign_patient_hospital($jwt, $patient_id, $hospital_id)
-
+    echo json_encode($response);
 }
 
 ?>
